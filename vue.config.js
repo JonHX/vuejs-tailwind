df66ed0dfddf0ddb693ada4a5dad9f2b78953ffb
jon tarrant <jon.tarrant@hotmail.co.uk>
2025-02-16 16:34:51 +0000

    renaming file

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  publicPath: '/your-repo-name/', // Your GitHub repo name
-  transpileDependencies: true, // Add this if necessary for transpiling dependencies
+module.exports = {
   css: {
     loaderOptions: {
       postcss: {
-        plugins: [require('tailwindcss'), require('autoprefixer')],
+        postcssOptions: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
       },
     },
   },
-});
+};
